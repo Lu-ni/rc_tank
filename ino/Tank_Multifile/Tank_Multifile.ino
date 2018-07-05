efa741cfde8901093bc96d50df27d601632ed7bc
Lu-ni <lucas.nicollier@gmail.com>
2018-07-05 11:35:15 +0200

    stop the tank if we lose the communication with the controler

--- ino/Tank_Multifile/Tank_Multifile.ino
+++ ino/Tank_Multifile/Tank_Multifile.ino
@@ -8,8 +8,6 @@ PS3BT PS3(&Btd);
 const int controlPin11 = 2; // connected to pin 7 on the H-bridge --- sens avant moteur 1
 const int controlPin12 = 3; // connected to pin 2 on the H-bridge --- sens arrière moteur 1
 const int enablePin1 = 5;   // connected to pin 1 on the H-bridge --- entrée pwm moteur 1
-
-
 const int controlPin21 = 21; // connected to pin 7 on the H-bridge --- sens avant moteur 2
 const int controlPin22 = 7; // connected to pin 2 on the H-bridge --- sens arrière moteur 2
 const int enablePin2 = 6;   // connected to pin 1 on the H-bridge --- entrée pwm moteur 2
@@ -28,7 +26,6 @@ void setup() {
     while (1); //halt
   }
   Serial.print(F("\r\nPS3 Bluetooth Library Started"));
-
   pinMode(controlPin11, OUTPUT);
   pinMode(controlPin12, OUTPUT);
   pinMode(enablePin1, OUTPUT);
@@ -36,15 +33,12 @@ void setup() {
   pinMode(controlPin22, OUTPUT);
   pinMode(enablePin2, OUTPUT);
 }
-
-
 /////////////////////////////////////////////////////////////////
 ////////////////////////////  LOOP  ////////////////////////////
 ///////////////////////////////////////////////////////////////
 void loop() {
   Usb.Task();
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
-
     if (PS3.getButtonClick(PS)) { //éteind la manette en appuyant sur le bouton PS (bouton du millieux)
       Serial.print(F("\r\nPS"));
       PS3.disconnect();
@@ -58,14 +52,8 @@ void loop() {
     } else {
       inactif();                                                            //action à faire quand aucune touche n'est pressée
     }
-  }//if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
-}//loop
-
-
-
-
-
-
-
-
- 
+  }//if (PS3.PS3Connected || PS3.PS3NavigationConnected)
+  else{
+  inactif();
+  }
+}//loop 
